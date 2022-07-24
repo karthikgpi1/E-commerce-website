8c884870415c9835b1c1660ed547f83b3618467b
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-24 16:39:33 +0530

    swiper slides

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ window.onscroll = () =>{
     searchForm.classList.remove('active');
     navbar.classList.remove('active');
 } 
+
+// home section swiper
+
+var swiper = new Swiper(".home-slider", {
+    
+    // spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
