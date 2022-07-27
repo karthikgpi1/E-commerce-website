ffb113918ffcd575dfaa8dc1fb57896979ab0bce
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-27 22:42:18 +0530

    blogs section part done

--- script.js
+++ script.js
@@ -143,37 +143,28 @@ var swiper = new Swiper(".review-slide", {
 // blogs swiper section starts
       
 var swiper = new Swiper(".blogs-slider", {
-    
-  // spaceBetween: 30,
   centeredSlides: true,
   loop:true,
   spaceBetween:20,
-
   autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
+    delay: 3000,
+    disableOnInteraction: false,
   },
-
   // navigation: {
   //   nextEl: ".swiper-button-next",
   //   prevEl: ".swiper-button-prev",
   // },
-
   breakpoints: {
-      0: {
-        slidesPerView: 1,
-      //   spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-      //   spaceBetween: 50,
-      },
-      1200: {
-        slidesPerView: 3,
-      //   spaceBetween: 50,
-      },
+    0: {
+      slidesPerView: 1,
     },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
 });
-
 // blogs swiper section ends
 
